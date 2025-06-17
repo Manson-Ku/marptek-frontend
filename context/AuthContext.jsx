6c18bf8ea5f92d2bee16aa3a41c824a397072a09
MansonKu <manson_ku@bpm.tw>
2025-06-17 12:21:11 +0800

    Refactor: decouple login and GBP authorization flow (C version)

--- context/AuthContext.jsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { createContext, useContext } from 'react'
-
-export const AuthContext = createContext({
-  customerId: null,
-  hasAccess: null,
-})
-
-export const useAuthContext = () => useContext(AuthContext)
