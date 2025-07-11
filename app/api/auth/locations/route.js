697fdb4060792ca60117c3cbf92f7b52b9228196
MansonKu <manson_ku@bpm.tw>
2025-07-11 16:24:56 +0800

    reply dev

--- app/api/auth/locations/route.js
+++ app/api/auth/locations/route.js
@@ -45,6 +45,7 @@ export async function GET(request) {
     FROM \`gbp-management-marptek.gbp_location.list\`
     WHERE customer_id = @customer_id
     ORDER BY upd_datetime DESC
+    LIMIT 200
   `
 
   try {
